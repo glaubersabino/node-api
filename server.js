5e83b38be6c6f2788116e19ef94fb1fb9c4ef4fe
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-08-24 16:30:13 -0300

    Implementação do Model Product e teste de cadastro manual no MongoDB.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express();
@@ -10,7 +11,16 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,
 });
 
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
 app.get("/", (req, res) => {
+  Product.create({
+    title: "React native",
+    description: "Teste de cadastro",
+    url: "https://github.com",
+  });
   res.send("Hello World. :)");
 });
 
