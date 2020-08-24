ff0715b04586b71b520d374662549d2bea9b77cc
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-08-24 16:44:25 -0300

    Refatoração da estrutura de diretórios e arquivos do projeto.

--- server.js
+++ server.js
@@ -13,15 +13,7 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
 
 requireDir("./src/models");
 
-const Product = mongoose.model("Product");
-
-app.get("/", (req, res) => {
-  Product.create({
-    title: "React native",
-    description: "Teste de cadastro",
-    url: "https://github.com",
-  });
-  res.send("Hello World. :)");
-});
+// Rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3000);
